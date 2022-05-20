9e96857d18299a3205fa10e48be49e3afe45dca6
alfman99 <amanresamntz@gmail.com>
2022-05-20 16:49:47 +0200

    script de equipo empezado

--- equip/info_equip.sh
+++ equip/info_equip.sh
@@ -2,13 +2,10 @@
 
 version_script="0.0.1"
 fecha_version="20/05/2022"
-nombre_fichero_output="log_abast.txt"
+nombre_fichero_output="log_equip.txt"
 
 fecha_inicio=$(date '+%Y-%m-%d') # fecha inicio del analisis
 hora_inicio=$(date '+%H:%M:%S') # hora inicio del analisis
-
-fecha_final=""
-hora_final=""
 
 comprobar_is_es_root() {
   if [ "$EUID" -ne 0 ]
@@ -91,13 +88,89 @@ imprimir_n_lineas() {
 
 # Imprimir al final, cuando tengamos todos los datos (fecha_final y hora_final)
 print_header_start() {
-  
-  text="$(printf "%s\n%s\n%s\n%s" " Anàlisi dels usuaris, ports i taules NF actius a l'equip realitzada per l'usuari root de l'equip $(cat /etc/hostname)." " Sistema operatiu $(grep ^NAME= /etc/os-release | cut -c7- | cut -d\" -f1) $(grep ^VERSION= /etc/os-release | cut -c10- | cut -d\" -f1)." " Versió del script $version_script compilada el $fecha_version." " Anàlisi iniciada en data $fecha_inicio a les $hora_inicio  i finalitzada en data $fecha_final a les $hora_final.)")"
+  fecha_final="$(date '+%Y-%m-%d')"
+  hora_final="$(date '+%H:%M:%S')"
+
+  text="$(printf "\n%s\n%s\n%s\n%s" " Anàlisi dels usuaris, ports i taules NF actius a l'equip realitzada per l'usuari root de l'equip $(cat /etc/hostname)." " Sistema operatiu $(grep ^NAME= /etc/os-release | cut -c7- | cut -d\" -f1) $(grep ^VERSION= /etc/os-release | cut -c10- | cut -d\" -f1)." " Versió del script $version_script compilada el $fecha_version." " Anàlisi iniciada en data $fecha_inicio a les $hora_inicio i finalitzada en data $fecha_final a les $hora_final.")"
   
   maxima_anchura="$(echo "$text" | wc -L)"
 
   imprimir_n_lineas "$maxima_anchura"
   echo "$text"
+  imprimir_n_lineas "$maxima_anchura"
+}
+
+print_usuarios_conectados_sistema() {
+
+  usuarios_activos=$(who | cut -d" " -f1 | tr '\n' ' ')
+  num_usuarios_activos="$(who | cut -d" " -f1 | wc -l)"
+
+  texto1="$num_usuarios_activos - $usuarios_activos"
+
+  text="$(printf "\n%-33s%s" "  Usuaris connectats al sistema:" "$texto1")"
+  text+="$(printf "\n       %-9s%-13s%-17s%s" "Usuari"   "Terminal"     "Establiment"      "Adreça")"
+
+  data_who=$(who)
+
+  while read usuario
+  do
+    text+="$(printf "\n       %s" "$usuario" )"
+  done < <(echo "$data_who")
+  
+  echo "$text"
+
+}
+
+print_usuarios_conexiones_activas() {
+  
+    usuarios_activos=$(who | cut -d" " -f1 | tr '\n' ' ')
+    num_usuarios_activos="$(who | cut -d" " -f1 | wc -l)"
+  
+    texto1="$num_usuarios_activos - $usuarios_activos"
+  
+    text="$(printf "\n%-33s%s" "  Usuaris connectats al sistema:" "$texto1")"
+    text+="$(printf "\n       %-9s%-13s%-17s%s" "Usuari"   "Terminal"     "Establiment"      "Adreça")"
+  
+    data_who=$(who)
+  
+    while read usuario
+    do
+      text+="$(printf "\n       %s" "$usuario" )"
+    done < <(echo "$data_who")
+    
+    echo "$text"
+  
+}
+
+print_usuarios_activos() {
+  
+  texto1="$(whoami)($(id -u)) [pts/$(id -u)]"
+  texto2="root(0) [pts/0]" #FALTA
+  texto3="1  - root(0)" #FALTA
+  texto4="2  - root[91], messagebus[1]" #FALTA
+
+  # Usuarios del sistema junto con su UID y PID
+  texto5="" #FALTA
+  users="$(cat "/etc/passwd" | tr ':' ' ' | awk '{print $1 " " $3}')"
+  while read line
+  do
+    usuario="$(echo "$line" | cut -d' ' -f1)"
+    id="$(echo "$line" | cut -d' ' -f2)"
+    texto5+="$(printf "%s(%s)," "$usuario" "$id") "
+  done < <(echo "$users")
+
+
+  text="$(printf "\n %-33s %s \n %-33s %s \n %-34s %s \n %-33s %s \n %-33s %s" " Usuari actual:" "$texto1" " Usuaris operatius:" "$texto2" " Usuaris amb accés root:" "$texto3" " Usuaris amb processos actius:" "$texto4" " Usuaris del sistema:" "$texto5")"
+  maxima_anchura="$(echo "$text" | wc -L)"
+
+  header="$(printf "\n%s" " Usuaris actius detectats al sistema")"
+
+  imprimir_n_lineas "$maxima_anchura"
+  echo "$header"
+  imprimir_n_lineas "$maxima_anchura"
+  echo "$text"
+  print_usuarios_conectados_sistema
+  print_usuarios_conexiones_activas
   imprimir_n_lineas "$maxima_anchura"
 
 }
@@ -109,4 +182,13 @@ parse_params "$@" # Parsea los parametros introducidos
 comprobar_paquetes_necesarios # Comprueba que estén instalados todos los paquetes necesarios para la ejecucion del programa
 
 
-# print_header_start
+
+
+
+{
+  print_header_start
+  echo -e "\n"
+  print_usuarios_activos
+  echo -e "\n"
+} > "$nombre_fichero_output"
+
