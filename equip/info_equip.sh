7d5f0ef122e7d9f716990a3c51899de7d94af136
alfman99 <amanresamntz@gmail.com>
2022-05-24 23:35:01 +0200

    oj

--- equip/info_equip.sh
+++ equip/info_equip.sh
@@ -100,6 +100,7 @@ print_header_start() {
   imprimir_n_lineas "$maxima_anchura"
 }
 
+# Hecho
 print_usuarios_conectados_sistema() {
 
   usuarios_activos=$(who | cut -d" " -f1 | tr '\n' ' ')
@@ -108,13 +109,13 @@ print_usuarios_conectados_sistema() {
   texto1="$num_usuarios_activos - $usuarios_activos"
 
   text="$(printf "\n%-33s%s" "  Usuaris connectats al sistema:" "$texto1")"
-  text+="$(printf "\n       %-9s%-13s%-17s%s" "Usuari"   "Terminal"     "Establiment"      "Adreça")"
+  text+="$(printf "\n          %-9s%-13s%-17s%s" "Usuari"   "Terminal"     "Establiment"      "Adreça")"
 
   data_who=$(who)
 
   while read usuario
   do
-    text+="$(printf "\n       %s" "$usuario" )"
+    text+="$(printf "\n          %s" "$usuario" )"
   done < <(echo "$data_who")
   
   echo "$text"
@@ -122,35 +123,68 @@ print_usuarios_conectados_sistema() {
 }
 
 print_usuarios_conexiones_activas() {
+
+  format="\n          %-15s%-26s%-12s%-26s%-17s"
+
+  datos_socket="$(ss -ptun)"
+  datos_procesos="$(ps -aux)" # la buena
+
+  datos=""
+
+  lista_usuarios=""
+
+  i=1
+  while read usuario
+  do
+    # if i == 1 next iteration and increment i, sirve para que no se imprima el header de la tabla
+    if [ "$i" -eq 1 ]; then
+      ((i++))
+      continue
+    fi
+    local_addr="$(echo "$usuario" | awk '{print $5}')"
+    remote_addr="$(echo "$usuario" | awk '{print $6}')"
+    pid="$(echo "$usuario" | awk '{print $7}' | sed -e 's/.*pid=\(.*\),.*/\1/')"
+
+    # buscar el proceso que corresponde al pid
+    proceso="$(echo "$datos_procesos" | grep "$pid" | head -n1)"
+
+    usuario="$(echo "$proceso" | awk '{print $1}')"
+    terminal="$(echo "$proceso" | awk '{print $7}')"
+
+    if [ "$usuario" == "USER" ]
+    then
+      continue
+    fi
+
+    lista_usuarios+=" $usuario"
+
+    datos+="$(printf "$format" "$usuario" "($local_addr" "<->" "$remote_addr" "$pid/$usuario@$terminal")"
+
+  done < <(echo "$datos_socket")
+
+  lista_usuarios="$(echo "$lista_usuarios" | tr ' ' '\n' | sort -u | tr '\n' ' ')"
+
+  text="$(printf "\n%-33s%s%s" "  Usuaris amb conexions actives:" "$(echo "$lista_usuarios" | xargs | tr ' ' '\n' | wc -l) -" "$lista_usuarios")"
+  text+="$(printf "$format" "Usuari" "(@IP:Port" "<->" "@IP:Port" "Procés:Usuari)")"
+  text+="$datos"
+
+  text=$(echo "$text" | uniq)
+
   
-    usuarios_activos=$(who | cut -d" " -f1 | tr '\n' ' ')
-    num_usuarios_activos="$(who | cut -d" " -f1 | wc -l)"
-  
-    texto1="$num_usuarios_activos - $usuarios_activos"
-  
-    text="$(printf "\n%-33s%s" "  Usuaris connectats al sistema:" "$texto1")"
-    text+="$(printf "\n       %-9s%-13s%-17s%s" "Usuari"   "Terminal"     "Establiment"      "Adreça")"
-  
-    data_who=$(who)
-  
-    while read usuario
-    do
-      text+="$(printf "\n       %s" "$usuario" )"
-    done < <(echo "$data_who")
-    
-    echo "$text"
-  
+  echo "$text"
+
+
 }
 
 print_usuarios_activos() {
   
   texto1="$(whoami)($(id -u)) [pts/$(id -u)]"
-  texto2="root(0) [pts/0]" #FALTA
-  texto3="1  - root(0)" #FALTA
-  texto4="2  - root[91], messagebus[1]" #FALTA
+  texto2="" #FALTA
+  texto3="" #FALTA
+  texto4="" #FALTA
 
   # Usuarios del sistema junto con su UID y PID
-  texto5="" #FALTA
+  texto5=""
   users="$(cat "/etc/passwd" | tr ':' ' ' | awk '{print $1 " " $3}')"
   while read line
   do
@@ -171,7 +205,77 @@ print_usuarios_activos() {
   echo "$text"
   print_usuarios_conectados_sistema
   print_usuarios_conexiones_activas
+  print_ssh_config
   imprimir_n_lineas "$maxima_anchura"
+
+}
+
+# DOING
+print_uso_mas_cpu() {
+
+  porcentaje="$1"
+
+  users_active="$(who)"
+
+  while read line
+  do
+    usuario="$(echo "$line" | awk '{print $1}')"
+    procesos="$(top -b -n 1 -u "$usuario")"
+
+    i=1
+    while read line
+    do
+      if [ "$i" -eq 1 ]; then # saltar la primera linea
+        ((i++))
+        continue
+      fi
+
+      # Contar porcentaje de CPU que está siendo utilizada por el usuario
+      porcentaje_cpu="$(echo "$line" | awk '{print $9}' | sed -e 's/%//')"
+
+
+      echo "$usuario"
+    done < <(echo "$procesos")
+
+    # Si el porcentaje de CPU calculado en el bucle anterior es mas grande que el parametro de entrada, añadir usuario a la lista para mostrarlo luego
+
+  done < <(echo "$users_active")
+
+
+}
+
+print_ssh_config() {
+
+  if [ -f "/etc/ssh/sshd_config" ]; then
+    value_info="$(grep ^PermitRootLogin= /etc/ssh/sshd_config)"
+
+    if [ -z "$value_info" ]; then
+      # No existe el valor, por lo que está por defecto el servidor 
+      value_info="Valor commentat"
+    else
+      value_info="$(echo "$value_info" | cut -d' ' -f2)"
+    fi
+    
+    text="$(printf "\n  %-17s %s" " SSH Usuari root permès:" "$value_info")"
+
+    users_allowed="$(grep ^AllowUsers= /etc/ssh/sshd_config | sed 's/[^ ]* //')" # Borrar todo antes del espacio, para quedarse solo con la lista de usuarios
+
+    if [ -z "$users_allowed" ]; then
+      # No existe el valor, por lo que está por defecto el servidor 
+      users_allowed="Valor commentat"
+    else
+      users_allowed="$(echo "$users_allowed")"
+    fi
+
+    text+="$(printf "\n  %-17s %s" " SSH Usuaris permesos:" "$users_allowed")"
+
+    echo -e "$text"
+
+
+  else 
+    echo "No hay servidor SSH en esta máquina."
+  fi
+
 
 }
 
@@ -198,7 +302,6 @@ print_nftables() {
 
 }
 
-
 # Empieza "main"
 comprobar_is_es_root
 parse_params "$@" # Parsea los parametros introducidos
@@ -209,12 +312,17 @@ comprobar_paquetes_necesarios # Comprueba que estén instalados todos los paquet
 
 
 {
-  print_header_start
-  echo -e "\n"
-  print_usuarios_activos
-  echo -e "\n\n"
-  print_ports_actius
-  echo -e "\n\n"
-  print_nftables
+  # print_header_start
+  # echo -e "\n"
+  # print_usuarios_activos
+  # echo -e "\n\n"
+  # print_ports_actius
+  # echo -e "\n\n"
+  # print_nftables
+  # echo -e "\n"
+  # print_usuarios_conexiones_activas
+
+  print_uso_mas_cpu
+
 } > "$nombre_fichero_output"
 
